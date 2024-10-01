1e6913b368602521fe25f8ea66f837909ac31fc6
AVANZIT\agomezm <adrian.gomez@ezentis.com>
2024-10-01 12:23:23 +0200

    feat (backend): SIG.57 Creamos el controlador de la tabla catalogo "Clase Suceso" con su correspondiente endpoint para listar la tabla

---
+++ EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.API/Controllers/ClaseSucesosController.cs
@@ -0,0 +1,32 @@
+﻿using DGPCE.Sigemad.Application.Features.ClasesSucesos.Quereis.GetClaseSucesosList;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net;
+
+namespace DGPCE.Sigemad.API.Controllers
+{
+        [Route("api/v1/[controller]")]
+        [ApiController]
+        public class ClaseSucesosController : ControllerBase
+        {
+            private readonly IMediator _mediator;
+
+            public ClaseSucesosController(IMediator mediator)
+            {
+                _mediator = mediator;
+            }
+
+            [HttpGet]
+            [ProducesResponseType((int)HttpStatusCode.OK)]
+            [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+            [SwaggerOperation(Summary = "Obtiene todos los tipos de sucesos")]
+            public async Task<IActionResult> GetAll()
+            {
+                var query = new GetClaseSucesosListQuery();
+                var listado = await _mediator.Send(query);
+                return Ok(listado);
+            }
+        }
+    }
+
