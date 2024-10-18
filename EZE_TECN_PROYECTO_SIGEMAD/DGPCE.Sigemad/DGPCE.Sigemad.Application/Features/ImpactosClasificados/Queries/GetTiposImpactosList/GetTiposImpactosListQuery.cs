936131453a88427bd9aaae3ea1c046c3c4c8adb9
laserrano <luisalberto.serrano@ezentis.com>
2024-10-18 10:20:54 +0200

    feat (backend): Test Tipos de impacto

--- EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.Application/Features/ImpactosClasificados/Queries/GetTiposImpactosList/GetTiposImpactosListQuery.cs
+++ EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.Application/Features/ImpactosClasificados/Queries/GetTiposImpactosList/GetTiposImpactosListQuery.cs
@@ -1,9 +1,4 @@
 ﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DGPCE.Sigemad.Application.Features.ImpactosClasificados.Queries.GetTiposImpactosList;
 public class GetTiposImpactosListQuery : IRequest<IReadOnlyList<string>>
--- EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.Application/Features/ImpactosClasificados/Queries/GetTiposImpactosList/GetTiposImpactosListQueryHandler.cs
+++ EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.Application/Features/ImpactosClasificados/Queries/GetTiposImpactosList/GetTiposImpactosListQueryHandler.cs
@@ -1,11 +1,6 @@
 ﻿using DGPCE.Sigemad.Application.Contracts.Persistence;
 using DGPCE.Sigemad.Domain.Modelos;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DGPCE.Sigemad.Application.Features.ImpactosClasificados.Queries.GetTiposImpactosList;
 public class GetTiposImpactosListQueryHandler : IRequestHandler<GetTiposImpactosListQuery, IReadOnlyList<string>>
@@ -19,7 +14,7 @@ public class GetTiposImpactosListQueryHandler : IRequestHandler<GetTiposImpactos
 
     public async Task<IReadOnlyList<string>> Handle(GetTiposImpactosListQuery request, CancellationToken cancellationToken)
     {
-        var tiposImpactos = await _unitOfWork.Repository<ImpactoClasificado>().GetAllAsync();
+        IReadOnlyList<ImpactoClasificado> tiposImpactos = await _unitOfWork.Repository<ImpactoClasificado>().GetAllAsync() ?? new List<ImpactoClasificado>();
         var tiposImpactosList = tiposImpactos
             .Select(t => t.TipoImpacto)
             .Distinct()
