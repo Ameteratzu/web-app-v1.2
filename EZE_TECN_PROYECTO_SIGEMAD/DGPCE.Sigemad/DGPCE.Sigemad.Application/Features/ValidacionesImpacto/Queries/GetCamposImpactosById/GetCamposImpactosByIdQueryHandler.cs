e6942e4d06e6b39c779aebdb0ee349f1b319bb9c
laserrano <luisalberto.serrano@ezentis.com>
2024-10-29 15:32:19 +0100

    feat (backend): Listado de campos de un impacto por id

---
+++ EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.Application/Features/ValidacionesImpacto/Queries/GetCamposImpactosById/GetCamposImpactosByIdQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using DGPCE.Sigemad.Application.Contracts.Persistence;
+using DGPCE.Sigemad.Application.Features.ValidacionesImpacto.Vms;
+using DGPCE.Sigemad.Domain.Modelos;
+using MediatR;
+
+namespace DGPCE.Sigemad.Application.Features.ValidacionesImpacto.Queries.GetCamposImpactosById;
+public class GetCamposImpactosByIdQueryHandler : IRequestHandler<GetCamposImpactosByIdQuery, IReadOnlyList<ValidacionImpactoClasificadoVm>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetCamposImpactosByIdQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<IReadOnlyList<ValidacionImpactoClasificadoVm>> Handle(GetCamposImpactosByIdQuery request, CancellationToken cancellationToken)
+    {
+        IReadOnlyList<ValidacionImpactoClasificado> camposImpactos = (await _unitOfWork.Repository<ValidacionImpactoClasificado>().GetAsync(m => m.IdImpactoClasificado == request.Id))
+            .OrderBy(m => m.Id)
+            .ToList()
+            .AsReadOnly();
+        var camposImpactosVm = _mapper.Map<IReadOnlyList<ValidacionImpactoClasificado>, IReadOnlyList<ValidacionImpactoClasificadoVm>>(camposImpactos);
+        return camposImpactosVm;
+    }
+}
---
+++ EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.Application/Features/ValidacionesImpacto/Vms/ValidacionImpactoClasificadoVm.cs
@@ -0,0 +1,8 @@
+﻿namespace DGPCE.Sigemad.Application.Features.ValidacionesImpacto.Vms;
+public class ValidacionImpactoClasificadoVm
+{
+    public int Id { get; set; }
+    public int IdImpactoClasificado { get; set; }
+    public string Campo { get; set; }
+    public bool EsObligatorio { get; set; }
+}
