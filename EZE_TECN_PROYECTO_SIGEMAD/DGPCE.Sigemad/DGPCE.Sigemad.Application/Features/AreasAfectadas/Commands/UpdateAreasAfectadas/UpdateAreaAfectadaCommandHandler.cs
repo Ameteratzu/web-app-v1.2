e78236b60667cc778a3c4cbeb1741ec3fca353c0
AVANZIT\agomezm <adrian.gomez@ezentis.com>
2024-10-31 17:33:43 +0100

    feat (Backend): Añadimos validacion de campos a la hora de actualizar area afectada

--- EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.Application/Features/AreasAfectadas/Commands/UpdateAreasAfectadas/UpdateAreaAfectadaCommandHandler.cs
+++ EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.Application/Features/AreasAfectadas/Commands/UpdateAreasAfectadas/UpdateAreaAfectadaCommandHandler.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using DGPCE.Sigemad.Application.Contracts.Persistence;
 using DGPCE.Sigemad.Application.Exceptions;
+using DGPCE.Sigemad.Application.Features.Evoluciones.Vms;
+using DGPCE.Sigemad.Domain.Constracts;
 using DGPCE.Sigemad.Domain.Modelos;
+using FluentValidation.Results;
 using MediatR;
 using Microsoft.Extensions.Logging;
 
@@ -11,14 +14,17 @@ internal class UpdateAreaAfectadaCommandHandler : IRequestHandler<UpdateAreaAfec
     private readonly ILogger<UpdateAreaAfectadaCommandHandler> _logger;
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
+    private readonly IGeometryValidator _geometryValidator;
 
     public UpdateAreaAfectadaCommandHandler(
         ILogger<UpdateAreaAfectadaCommandHandler> logger,
         IUnitOfWork unitOfWork,
+        IGeometryValidator geometryValidator,
         IMapper mapper
         )
     {
         _logger = logger;
+        _geometryValidator = geometryValidator;
         _unitOfWork = unitOfWork;
         _mapper = mapper;
     }
@@ -31,6 +37,36 @@ internal class UpdateAreaAfectadaCommandHandler : IRequestHandler<UpdateAreaAfec
         {
             _logger.LogWarning($"No se encontro area afectada con id: {request.Id}");
             throw new NotFoundException(nameof(AreaAfectada), request.Id);
+        }
+
+        var evolucion = await _unitOfWork.Repository<Evolucion>().GetByIdAsync(request.IdEvolucion);
+        if (evolucion is null)
+        {
+            _logger.LogWarning($"request.IdEvolucion: {request.IdEvolucion}, no encontrado");
+            throw new NotFoundException(nameof(Evolucion), request.IdEvolucion);
+        }
+
+        var provincia = await _unitOfWork.Repository<Provincia>().GetByIdAsync(request.IdProvincia);
+        if (provincia is null)
+        {
+            _logger.LogWarning($"request.IdProvincia: {request.IdProvincia}, no encontrado");
+            throw new NotFoundException(nameof(Provincia), request.IdProvincia);
+        }
+
+        var municipio = await _unitOfWork.Repository<Municipio>().GetByIdAsync(request.IdMunicipio);
+        if (municipio is null)
+        {
+            _logger.LogWarning($"request.IdMunicipio: {request.IdMunicipio}, no encontrado");
+            throw new NotFoundException(nameof(Municipio), request.IdMunicipio);
+        }
+
+        if (!_geometryValidator.IsGeometryValidAndInEPSG4326(request.GeoPosicion))
+        {
+            ValidationFailure validationFailure = new ValidationFailure();
+            validationFailure.ErrorMessage = "No es una geometria valida o no tiene el EPS4326";
+
+            _logger.LogWarning($"{validationFailure}, geometria -> {request.GeoPosicion}");
+            throw new ValidationException(new List<ValidationFailure> { validationFailure });
         }
 
         _mapper.Map(request, areaAfectada, typeof(UpdateAreaAfectadaCommand), typeof(AreaAfectada));
