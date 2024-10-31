a8f25d5f77ecd1d794da7c6ae52ad3f99c395885
AVANZIT\agomezm <adrian.gomez@ezentis.com>
2024-10-31 10:06:07 +0100

    Cambiamos la visibilidad de la clase ValidationMessages de internal a public

--- EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.Application/Resources/ValidationMessages.Designer.cs
+++ EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.Application/Resources/ValidationMessages.Designer.cs
@@ -22,7 +22,7 @@ namespace DGPCE.Sigemad.Application.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class ValidationMessages {
+    public class ValidationMessages {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
