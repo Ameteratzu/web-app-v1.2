58551b253c4f4e081c9316faff422938987a0a92
AVANZIT\agomezm <adrian.gomez@ezentis.com>
2024-11-05 11:50:05 +0100

    feat (Backend): SIG-128 Creacion API para crear una nueva Direccion Coordinación Emergencia

---
+++ EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.Application/Specifications/DireccionCoordinacionEmergencias/DireccionCoordinacionEmergenciaByIdSpecification.cs
@@ -0,0 +1,15 @@
+﻿using DGPCE.Sigemad.Domain.Modelos;
+
+
+namespace DGPCE.Sigemad.Application.Specifications.DireccionCoordinacionEmergencias;
+
+
+
+public class DireccionCoordinacionEmergenciaByIdSpecification : BaseSpecification<DireccionCoordinacionEmergencia>
+{
+    public DireccionCoordinacionEmergenciaByIdSpecification(int id)
+        : base(e => e.Id == id && e.Borrado == false)
+    {
+        AddInclude(e => e.ActivacionPlanEmergencia);
+    }
+}
