07b6f0da312e66653b824e2f4bedd872155e4fe3
AVANZIT\agomezm <adrian.gomez@ezentis.com>
2024-11-20 10:54:28 +0100

    Feat (Backend) #89 Realizamos el endpoint para listar fases de una evolucion

---
+++ EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.API/Controllers/FasesController.cs
@@ -0,0 +1,32 @@
+﻿using DGPCE.Sigemad.API.Constants;using DGPCE.Sigemad.Application.Features.Fases.Queries;
+using DGPCE.Sigemad.Domain.Modelos;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net;
+
+namespace DGPCE.Sigemad.API.Controllers;
+
+
+[ApiController]
+[Route("/api/v1/fases")]
+public class FasesController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public FasesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [ProducesResponseType((int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+    [SwaggerOperation(Tags = new[] { SwaggerTags.Maestros }, Summary = "Obtiene el listado competo de fases")]
+    public async Task<ActionResult<IReadOnlyList<Fase>>> GetAll()
+    {
+        var query = new GetFasesListQuery();
+        var listado = await _mediator.Send(query);
+        return Ok(listado);
+    }
+}
---
+++ EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.Application/Features/Fases/Queries/GetFasesListQuery.cs
@@ -0,0 +1,8 @@
+﻿using DGPCE.Sigemad.Domain.Modelos;
+using MediatR;
+
+
+namespace DGPCE.Sigemad.Application.Features.Fases.Queries;
+public class GetFasesListQuery : IRequest<IReadOnlyList<Fase>>
+{
+}
