868e1a16bf15c600cc17790c0ffd4682fee748f6
Francisco Morales <fjmorales.caamano_ord@ezentis.com>
2024-11-25 17:30:44 -0500

    Merge app.componets

--- EZE_TECN_PROYECTO_SIGEMAD/Sigemad_Front/src/app/app.routes.ts
+++ EZE_TECN_PROYECTO_SIGEMAD/Sigemad_Front/src/app/app.routes.ts
@@ -1,11 +1,30 @@
 import { Routes } from '@angular/router';
-
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { FireEditComponent } from './pages/fire/fire-edit/fire-edit.component';
+import { CommentsComponent } from './pages/comments/comments.component';
 import { FireComponent } from './pages/fire/fire.component';
+import { FireEditComponent } from './pages/fire/fire-edit/fire-edit.component';
 
 export const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'fire', component: FireComponent },
-  { path: 'fire-national-edit/:id', component: FireEditComponent },
+
+    { path: '*', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'fire', component: FireComponent },
+    { path: 'earthquakes', component: CommentsComponent },
+    { path: 'adverse-weather', component: CommentsComponent },
+    { path: 'volcanic-phenomena', component: CommentsComponent },
+    { path: 'floods', component: CommentsComponent },
+    { path: 'chemical-risk', component: CommentsComponent },
+    { path: 'dangerous-goods', component: CommentsComponent },
+    { path: 'nuclear-radiological-risk', component: CommentsComponent },
+    { path: 'other-risks', component: CommentsComponent },
+    { path: 'ope', component: CommentsComponent },
+    { path: 'documentation', component: CommentsComponent },
+    { path: 'incidents', component: CommentsComponent },
+    { path: 'config', component: CommentsComponent },
+    { path: 'users', component: CommentsComponent },
+    { path: 'catalogs', component: CommentsComponent },
+    { path: 'search', component: CommentsComponent },
+    { path: 'episodes', component: CommentsComponent },
+    { path: 'fire-national-edit/:id', component: FireEditComponent },
 ];
+
