e3e83d9ff1df45e671faaf4fa644c48f41a9cdfa
Kevin Palomino <kevin.palomino_ext@ezentis.com>
2024-11-28 01:44:32 +0100

    Tabla Registro de actualizacion

--- EZE_TECN_PROYECTO_SIGEMAD/Sigemad_Front/src/app/pages/fire/fire-edit/fire-edit.component.ts
+++ EZE_TECN_PROYECTO_SIGEMAD/Sigemad_Front/src/app/pages/fire/fire-edit/fire-edit.component.ts
@@ -1,19 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal, ViewChild } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
 
-import { ActivatedRoute, Router } from '@angular/router';
-
-
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatGridListModule } from '@angular/material/grid-list';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { FlexLayoutModule } from '@angular/flex-layout'; 
-
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 import {
   FormControl,
@@ -22,6 +22,7 @@ import {
   ReactiveFormsModule,
 } from '@angular/forms';
 
+import moment from 'moment';
 
 import { EventService } from '../../../services/event.service';
 import { EventStatusService } from '../../../services/eventStatus.service';
@@ -31,31 +32,19 @@ import { MenuItemActiveService } from '../../../services/menu-item-active.servic
 import { MunicipalityService } from '../../../services/municipality.service';
 import { ProvinceService } from '../../../services/province.service';
 
-
 import { Event } from '../../../types/event.type';
-import { FireDetail } from '../../../types/fire-detail.type';
+
+import { EventStatus } from '../../../types/eventStatus.type';
 import { FireStatus } from '../../../types/fire-status.type';
 import { Fire } from '../../../types/fire.type';
 import { Municipality } from '../../../types/municipality.type';
 import { Province } from '../../../types/province.type';
-import { EventStatus } from '../../../types/eventStatus.type';
-
 
 import { FormFieldComponent } from '../../../shared/Inputs/field.component';
+import { FireCoordinationData } from '../../fire-coordination-data/fire-coordination-data.component';
+import { FireDocumentation } from '../../fire-documentation/fire-documentation.component';
 import { FireCreateComponent } from '../../fire-evolution-create/fire-evolution-create.component';
 import { FireOtherInformationComponent } from '../../fire-other-information/fire-other-information.component';
-import { FireDocumentation } from '../../fire-documentation/fire-documentation.component';
-import { FireCoordinationData } from '../../fire-coordination-data/fire-coordination-data.component';
-
-
-import moment from 'moment';
-import Feature from 'ol/Feature';
-import { Geometry } from 'ol/geom';
-
-// import { FireDirectionCoordinationComponent } from '../components/fire-direction-coordination/fire-direction-coordination.component';
-// import { MapCreateComponent } from '../../../shared/mapCreate/map-create.component';
-// import { FireEvolutionCreateComponent } from '../../fire-evolution-create/fire-evolution-create.component';
-//import { FireEvolutionCreateComponent } from '../../fire-evolution-create/fire-evolution-create.component';
 
 @Component({
   selector: 'app-fire-edit',
@@ -72,31 +61,26 @@ import { Geometry } from 'ol/geom';
     MatButtonModule,
     MatCardModule,
     MatDividerModule,
-    MatDialogModule
+    MatDialogModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
   providers: [],
   templateUrl: './fire-edit.component.html',
   styleUrl: './fire-edit.component.scss',
 })
-export class FireEditComponent {
-  featuresCoords: Feature<Geometry>[] = [];
+export class FireEditComponent implements OnInit {
+  @ViewChild(MatSort) sort!: MatSort;
 
-  classValidate = signal<string>('needs-validation');
-
-
-  public router = inject(Router);
-
-  public route = inject(ActivatedRoute);
-
+  public activedRoute = inject(ActivatedRoute);
   public matDialog = inject(MatDialog);
-
   public menuItemActiveService = inject(MenuItemActiveService);
   public fireService = inject(FireService);
   public provinceService = inject(ProvinceService);
   public municipalityService = inject(MunicipalityService);
   public eventService = inject(EventService);
   public eventStatusService = inject(EventStatusService);
-  
   public fireStatusService = inject(FireStatusService);
 
   public fire = <Fire>{};
@@ -105,84 +89,44 @@ export class FireEditComponent {
   public events = signal<Event[]>([]);
   public eventsStatus = signal<EventStatus[]>([]);
   public fireStatus = signal<FireStatus[]>([]);
-  public logs = signal<FireDetail[]>([]);
 
   public formData!: FormGroup;
 
-  public error: boolean = false;
+  public dataSource = new MatTableDataSource<any>([]);
 
-  public showUpdateLog: boolean = true;
-  public showDetailsUpdate: boolean = false;
-
-  public details = [
-    {
-      reg: '10',
-      datetime: '19/08/2024 19:45',
-      scope: 'Personas',
-      type: 'Evacuados',
-      implication: 'Santa María (134)',
-    },
-    {
-      reg: '10',
-      datetime: '19/08/2024 19:25',
-      scope: 'Viabilidad',
-      type: 'Meteorológica',
-      implication: 'CN-21 (Corte PK 2,300-3,100)',
-    },
-    {
-      reg: '9',
-      datetime: '19/08/2024 19:15',
-      scope: 'Medios estatales',
-      type: 'Extraordinario',
-      implication: 'UME (Aprobación - Salida)',
-    },
-    {
-      reg: '8',
-      datetime: '19/08/2024 19:12',
-      scope: 'Medios estatales',
-      type: 'Extraordinario',
-      implication: 'UME (Aprobación - Entrada)',
-    },
-    {
-      reg: '7',
-      datetime: '19/08/2024 18:15',
-      scope: 'Dirección',
-      type: 'Entrada',
-      implication: 'COCOPI (Inicio)',
-    },
-    {
-      reg: '6',
-      datetime: '19/08/2024 18:10',
-      scope: 'Medios estatales',
-      type: 'Extraordinario',
-      implication: 'UME (Solicitud - Salida)',
-    },
+  public displayedColumns: string[] = [
+    'numero',
+    'fechaHora',
+    'registro',
+    'origen',
+    'tipoRegistro',
+    'tecnico',
   ];
 
   async ngOnInit() {
-    localStorage.removeItem('coordinates');
-
     this.menuItemActiveService.set.emit('/fire');
 
     this.formData = new FormGroup({
       id: new FormControl(),
-      denomination: new FormControl({ value: '', disabled: true }), 
+      denomination: new FormControl({ value: '', disabled: true }),
       territory: new FormControl(),
       province: new FormControl(),
       municipality: new FormControl(),
-      startDate: new FormControl({  value: '', disabled: true }),  
+      startDate: new FormControl({ value: '', disabled: true }),
       event: new FormControl(),
       generalNote: new FormControl(),
       idEstado: new FormControl(),
-      ubicaciones: new FormControl({  value: '', disabled: true }),
-      suceso: new FormControl({  value: '', disabled: true }),
-      estado: new FormControl({  value: '', disabled: true }),    
+      ubicaciones: new FormControl({ value: '', disabled: true }),
+      suceso: new FormControl({ value: '', disabled: true }),
+      estado: new FormControl({ value: '', disabled: true }),
     });
 
-    const fire_id = Number(this.route.snapshot.paramMap.get('id'));
+    this.dataSource.data = [];
+
+    const fire_id = Number(this.activedRoute.snapshot.paramMap.get('id'));
 
     const fire = await this.fireService.getById(fire_id);
-    console.log("🚀 ~ FireEditComponent ~ ngOnInit ~ fire:", fire)
+
     this.fire = fire;
     const provinces = await this.provinceService.get();
     this.provinces.set(provinces);
@@ -202,14 +146,14 @@ export class FireEditComponent {
     this.fireStatus.set(fireStatus);
 
     const details = await this.fireService.details(Number(fire_id));
-    this.logs.set(details);
+    this.dataSource.data = details;
 
     this.formData.patchValue({
       id: this.fire.id,
       territory: this.fire.idTerritorio,
       denomination: this.fire.denominacion,
       province: this.fire.idProvincia,
-      municipality: this.fire.id ,
+      municipality: this.fire.id,
       startDate: moment(this.fire.fechaInicio).format('YYYY-MM-DD'),
       event: this.fire.idClaseSuceso,
       generalNote: this.fire.notaGeneral,
@@ -218,8 +162,10 @@ export class FireEditComponent {
       suceso: this.fire.claseSuceso?.descripcion,
       estado: this.fire.estadoSuceso?.descripcion,
     });
-   
-    //this.openModalEvolution()
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
 
   async loadMunicipalities(event: any) {
@@ -228,152 +174,17 @@ export class FireEditComponent {
     this.municipalities.set(municipalities);
   }
 
-  async onSubmit() {
-    if (this.formData.invalid) {
-      this.formData.markAllAsTouched();
-      return 
-    }
-    
-    this.error = false;
-    const data = this.formData.value;
-
-    
-
-    if (this.featuresCoords.length) {
-      data.coordinates = this.featuresCoords;
-    } else {
-      const municipio = this.municipalities().find(
-        (item) => item.id === this.formData.value.municipality
-      );
-
-      data.coordinates = [
-        municipio?.geoPosicion?.coordinates[1],
-        municipio?.geoPosicion?.coordinates[0],
-      ];
-    }
-
-    await this.fireService
-      .update(data)
-      .then((response) => {
-        // this.messageService.add({
-        //   severity: 'success',
-        //   summary: 'Modificado',
-        //   detail: 'Incendio modificado correctamente',
-        // });
-
-        new Promise((resolve) => setTimeout(resolve, 2000)).then(() =>
-          this.router.navigate([`/fire`])
-        );
-      })
-      .catch((error) => {
-        this.error = true;
-      });
-  }
-
-  async confirmDelete() {
-    if (confirm('¿Está seguro que desea eliminar este incendio?')) {
-      const fire_id = Number(this.route.snapshot.paramMap.get('id'));
-
-      await this.fireService
-        .delete(fire_id)
-        .then((response) => {
-          // this.messageService.add({
-          //   severity: 'success',
-          //   summary: 'Eliminado',
-          //   detail: 'Incendio eliminado correctamente',
-          // });
-          new Promise((resolve) => setTimeout(resolve, 2000)).then(
-            () => (window.location.href = '/fire')
-          );
-        })
-        .catch((error) => {
-          this.error = true;
-        });
-    }
-  }
-
-  // openModalMapEdit() {
-  //   const municipio = this.municipalities().find(
-  //     (item) => item.id === this.formData.value.municipality
-  //   );
-
-  //   const dialogRef = this.matDialog.open(MapCreateComponent, {
-  //     width: '780px',
-  //     maxWidth: '780px',
-  //     height: '780px',
-  //     maxHeight: '780px',
-  //     data: { municipio: municipio, listaMunicipios: this.municipalities() },
-  //   });
-
-  //   dialogRef.componentInstance.save.subscribe(
-  //     (features: Feature<Geometry>[]) => {
-  //       this.featuresCoords = features;
-  //       console.info('this.featuresCoords', this.featuresCoords);
-  //     }
-  //   );
-  // }
-
-  // openModalEvolution() {
-  //   let evolutionModalRef = this.matDialog.open(FireEvolutionCreateComponent, {
-  //     width: '1220px',
-  //     maxWidth: '1220px',
-  //     height: '720px',
-  //     disableClose: true,
-  //   });
-
-  //   evolutionModalRef.componentInstance.fire_id = Number(
-  //     this.route.snapshot.paramMap.get('id')
-  //   );
-  // }
-
-  // openModalDireccion() {
-  //   let evolutionModalRef = this.matDialog.open(FireDirectionCoordinationComponent, {
-  //     width: '1220px',
-  //     maxWidth: '1220px',
-  //     height: '720px',
-  //     disableClose: true,
-  //   });
-
-    
-    
-  //}
-
-  showTable(table: string) {
-    this.showUpdateLog = false;
-    this.showDetailsUpdate = false;
-
-    if (table == 'showUpdateLog') {
-      this.showUpdateLog = true;
-    }
-
-    if (table == 'showDetailsUpdate') {
-      this.showDetailsUpdate = true;
-    }
-  }
-
-  back() {
-    this.router.navigate([`/fire`]);
-  }
-
-  getEstadoDesc(fire: Fire) {
-    const desc = fire?.estadoSuceso?.descripcion
-      ? fire?.estadoSuceso?.descripcion
-      : '';
-    return;
-    desc;
-  }
-
   getForm(atributo: string): any {
     return this.formData.controls[atributo];
   }
 
   goModalEvolution() {
     const dialogRef = this.matDialog.open(FireCreateComponent, {
-      width: '90vw', 
-      height: '90vh', 
-      maxWidth: 'none', 
+      width: '90vw',
+      height: '90vh',
+      maxWidth: 'none',
       data: {
-        title: 'Nuevo - Datos Evolución', 
+        title: 'Nuevo - Datos Evolución',
       },
     });
 
@@ -386,11 +197,11 @@ export class FireEditComponent {
 
   goModalCoordination() {
     const dialogRef = this.matDialog.open(FireCoordinationData, {
-      width: '90vw', 
-      height: '90vh', 
-      maxWidth: 'none', 
+      width: '90vw',
+      height: '90vh',
+      maxWidth: 'none',
       data: {
-        title: 'Nuevo - Datos de dirección y coordinación de la emergencia', 
+        title: 'Nuevo - Datos de dirección y coordinación de la emergencia',
       },
     });
 
@@ -401,15 +212,13 @@ export class FireEditComponent {
     });
   }
 
-  
-
   goModalOtherInformation() {
     const dialogRef = this.matDialog.open(FireOtherInformationComponent, {
-      width: '90vw', 
-      maxWidth: 'none', 
-      //height: '90vh', 
+      width: '90vw',
+      maxWidth: 'none',
+      //height: '90vh',
       data: {
-        title: 'Nuevo - Otra Información', 
+        title: 'Nuevo - Otra Información',
       },
     });
 
@@ -422,11 +231,11 @@ export class FireEditComponent {
 
   goModalDocumentation() {
     const dialogRef = this.matDialog.open(FireDocumentation, {
-      width: '90vw', 
-      maxWidth: 'none', 
-      //height: '90vh', 
+      width: '90vw',
+      maxWidth: 'none',
+      //height: '90vh',
       data: {
-        title: 'Documentación', 
+        title: 'Documentación',
       },
     });
 
