e31353dbb2aebf36b0829f2b76c7fb3b81f3c23b
AVANZIT\agomezm <adrian.gomez@ezentis.com>
2024-11-28 11:30:07 +0100

    Feat (Backend) ajustamos el json de la busqueda de detalles de documentacion

--- EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.Application/Dtos/DetallesDocumentaciones/DetalleDocumentacionBusquedaDto.cs
+++ EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.Application/Dtos/DetallesDocumentaciones/DetalleDocumentacionBusquedaDto.cs
@@ -1,4 +1,5 @@
 ﻿using DGPCE.Sigemad.Application.Dtos.ProcedenciasDestinos;
+using DGPCE.Sigemad.Domain.Modelos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,7 @@ public class DetalleDocumentacionBusquedaDto
     public int Id { get; set; }
     public DateTime FechaHora { get; set; }
     public DateTime FechaHoraSolicitud { get; set; }
-    public int IdTipoDocumento { get; set; }
+    public TipoDocumento TipoDocumento { get; set; }
     public string Descripcion { get; set; }
     public Guid? IdArchivo { get; set; }
 
--- EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.Application/Specifications/Documentos/DetalleDocumentacionById.cs
+++ EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.Application/Specifications/Documentos/DetalleDocumentacionById.cs
@@ -11,8 +11,9 @@ public class DetalleDocumentacionById : BaseSpecification<Documentacion>
     public DetalleDocumentacionById(int id)
         : base(e => e.Id == id && e.Borrado == false)
     {
-        AddInclude(i => i.DetallesDocumentacion);
+        AddInclude(i => i.DetallesDocumentacion); 
         AddInclude("DetallesDocumentacion.DocumentacionProcedenciaDestinos.ProcedenciaDestino");
+        AddInclude("DetallesDocumentacion.TipoDocumento");
 
     }
 }
