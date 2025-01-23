ca0576dc999d2083fbd7374a3249bf10f4cf8833
Francisco Morales <fjmorales.caamano_ord@ezentis.com>
2025-01-23 09:27:07 -0500

    fix env

---
+++ EZE_TECN_PROYECTO_SIGEMAD/Sigemad_Front/src/app/pages/fire-actions-relevant/fire-actions-relevant.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fire-actions-relevant',
+  standalone: true,
+  imports: [],
+  templateUrl: './fire-actions-relevant.component.html',
+  styleUrl: './fire-actions-relevant.component.scss'
+})
+export class FireActionsRelevantComponent {
+
+}
