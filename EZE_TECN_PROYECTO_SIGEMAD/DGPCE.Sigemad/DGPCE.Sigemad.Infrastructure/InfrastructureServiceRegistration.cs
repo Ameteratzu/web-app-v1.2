927398cc32bd529dcb87812ed026e4bbb1b5f839
Romario <romario.vargas_ext@ezentis.com>
2025-03-21 13:48:53 -0500

    feat (frontend-backend): Se agrego cache en memoria y definio color de mensaje al terminar una sesion

--- EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.Infrastructure/InfrastructureServiceRegistration.cs
+++ EZE_TECN_PROYECTO_SIGEMAD/DGPCE.Sigemad/DGPCE.Sigemad.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,6 +1,8 @@
-﻿using DGPCE.Sigemad.Application.Contracts.Files;
+﻿using DGPCE.Sigemad.Application.Contracts.Caching;
+using DGPCE.Sigemad.Application.Contracts.Files;
 using DGPCE.Sigemad.Application.Contracts.Persistence;
 using DGPCE.Sigemad.Domain.Constracts;
+using DGPCE.Sigemad.Infrastructure.Caching;
 using DGPCE.Sigemad.Infrastructure.Options;
 using DGPCE.Sigemad.Infrastructure.Persistence;
 using DGPCE.Sigemad.Infrastructure.Repositories;
@@ -10,32 +12,36 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NetTopologySuite.Geometries;
 
-namespace DGPCE.Sigemad.Infrastructure
+namespace DGPCE.Sigemad.Infrastructure;
+
+public static class InfrastructureServiceRegistration
 {
-    public static class InfrastructureServiceRegistration
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
-        {
-            DatabaseSettings? dbSettings = configuration.GetSection("DatabaseSettings")
-                .Get<DatabaseSettings>();
+        // Configuracion de cache
+        services.AddMemoryCache();
+        services.AddScoped<ISIGEMemoryCache, SIGEMemoryCache>();
 
-            services.AddDbContext<SigemadDbContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("ConnectionString"),
-                options => options.UseNetTopologySuite())
-                .EnableSensitiveDataLogging(dbSettings.EnableSensitiveDataLogging) // Incluye datos sensibles en los logs (¡solo para desarrollo!)
-                .EnableDetailedErrors(dbSettings.EnableDetailedErrors) // Habilita errores detallados
-            );
+        // Configuración de la base de datos
+        DatabaseSettings? dbSettings = configuration.GetSection("DatabaseSettings")
+            .Get<DatabaseSettings>();
 
-            services.AddSingleton<GeometryFactory>(NetTopologySuite.Geometries.GeometryFactory.Default);
+        services.AddDbContext<SigemadDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("ConnectionString"),
+            options => options.UseNetTopologySuite())
+            .EnableSensitiveDataLogging(dbSettings.EnableSensitiveDataLogging) // Incluye datos sensibles en los logs (¡solo para desarrollo!)
+            .EnableDetailedErrors(dbSettings.EnableDetailedErrors) // Habilita errores detallados
+        );
 
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
-            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
-            services.AddScoped<IGeometryValidator, GeometryValidator>();
-            services.AddScoped<ICoordinateTransformationService, CoordinateTransformationService>();
-            services.AddTransient<IFileService, LocalFileService> (provider => new LocalFileService(configuration));
+        services.AddSingleton<GeometryFactory>(NetTopologySuite.Geometries.GeometryFactory.Default);
 
-            return services;
-        }
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+        services.AddScoped<IGeometryValidator, GeometryValidator>();
+        services.AddScoped<ICoordinateTransformationService, CoordinateTransformationService>();
+        services.AddTransient<IFileService, LocalFileService>(provider => new LocalFileService(configuration));
 
+        return services;
     }
+
 }
